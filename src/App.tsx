579e3b7e3e1fc220491829b0fa80d95e3d143a24
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2022-02-03 16:55:45 +0100

    branching: configuring v.2

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,11 @@ function App() {
   return (
     <div className="App">
      <h1>hello world</h1>
+<<<<<<< HEAD
      <h2>who the fuck is that!</h2>
+=======
+     <h2>boo</h2>
+>>>>>>> d4ff420 (branching: configuring)
     </div>
   );
 }
