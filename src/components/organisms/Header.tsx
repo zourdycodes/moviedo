dea67fef9461386bc9f3010966b9c107da8104c7
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2022-02-04 14:19:29 +0100

    feat-styling: checking the shrink and add some touch upon shrinking changes

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 import logo from "../../assets/tmovie.png";
@@ -20,9 +20,26 @@ const headerNav = [
 
 export const Header: React.FC = () => {
   const { pathname } = useLocation();
-  const headerRef = useRef(null);
+  const headerRef = useRef<HTMLDivElement>(null);
 
   const active = headerNav.findIndex((e) => e.path === pathname);
+
+  useEffect(() => {
+    const shrinkHeader = () => {
+      if (
+        document.body.scrollTop > 100 ||
+        document.documentElement.scrollTop > 100
+      ) {
+        headerRef.current?.classList.add("shrink");
+      } else {
+        headerRef.current?.classList.remove("shrink");
+      }
+    };
+
+    window.addEventListener("scroll", shrinkHeader);
+
+    return () => window.removeEventListener("scroll", shrinkHeader);
+  }, []);
 
   return (
     <div ref={headerRef} className="header">
