4cdb196925fcab56d43917dc58bf89fcf49aa78e
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2022-02-04 15:19:25 +0100

    feat-styling: styling the footer

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,9 +1,41 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
+import logo from "../../assets/tmovie.png";
+import bgImage from "../../assets/footer-bg.jpg";
+
 export const Footer: React.FC = () => {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="footer" style={{ backgroundImage: `url(${bgImage})` }}>
+      <div className="footer__content container">
+        <div className="footer__content__logo">
+          <div className="logo">
+            <img src={logo} alt="Moviedo" />
+            <Link to="/">tMovies</Link>
+          </div>
+        </div>
+
+        <div className="footer__content__menus">
+          <div className="footer__content__menu">
+            <Link to="/">Home</Link>
+            <Link to="/">Contact us</Link>
+            <Link to="/">Term of services</Link>
+            <Link to="/">About us</Link>
+          </div>
+          <div className="footer__content__menu">
+            <Link to="/">Live</Link>
+            <Link to="/">FAQ</Link>
+            <Link to="/">Premium</Link>
+            <Link to="/">Pravacy policy</Link>
+          </div>
+          <div className="footer__content__menu">
+            <Link to="/">You must watch</Link>
+            <Link to="/">Recent release</Link>
+            <Link to="/">Top IMDB</Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
